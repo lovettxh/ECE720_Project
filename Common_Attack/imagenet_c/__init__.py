216a37417bf392d78e33b49ee9327bf6e09fc160
wollf2008 <43559182+wollf2008@users.noreply.github.com>
2022-03-25 17:35:02 -0600

    Add files via upload

--- Common_Attack/imagenet_c/__init__.py
+++ Common_Attack/imagenet_c/__init__.py
@@ -1,5 +1,8 @@
 import numpy as np
 from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from .corruptions import *
 
 corruption_tuple = (gaussian_noise, shot_noise, impulse_noise, defocus_blur,
@@ -10,7 +13,10 @@ corruption_tuple = (gaussian_noise, shot_noise, impulse_noise, defocus_blur,
 corruption_dict = {corr_func.__name__: corr_func for corr_func in corruption_tuple}
 
 
-def corrupt(x, severity=1, corruption_name=None, corruption_number=-1):
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+
+def corrupt(x, severity=2, corruption_name=None, corruption_number=-1):
     """
     :param x: image to corrupt; a 224x224x3 numpy array in [0, 255]
     :param severity: strength with which to corrupt x; an integer in [0, 5]
@@ -24,12 +30,37 @@ def corrupt(x, severity=1, corruption_name=None, corruption_number=-1):
     an integer in [0, 18]; useful for easy looping; 15, 16, 17, 18 are validation corruption numbers
     :return: the image x corrupted by a corruption function at the given severity; same shape as input
     """
+    if len(x[0,0]) == 1:
+        x_rgb = np.concatenate((x, x, x), axis=-1)
+    else:
+        x_rgb = x
+
+    float_input = 0
+    if "uint8" in str(np.dtype(x_rgb[0,0,0])):
+        x_rgb = x_rgb
+    elif "float32" in str(np.dtype(x_rgb[0,0,0])):
+        float_input = 1
+        x_rgb = np.uint8(x_rgb * 255.0)
+    else:
+        raise ValueError("Input should be a h*w*3 numpy array in uint8 or float32")
 
     if corruption_name:
-        x_corrupted = corruption_dict[corruption_name](Image.fromarray(x), severity)
+        x_corrupted = corruption_dict[corruption_name](Image.fromarray(x_rgb), severity)
     elif corruption_number != -1:
-        x_corrupted = corruption_tuple[corruption_number](Image.fromarray(x), severity)
+        x_corrupted = corruption_tuple[corruption_number](Image.fromarray(x_rgb), severity)
     else:
         raise ValueError("Either corruption_name or corruption_number must be passed")
 
-    return np.uint8(x_corrupted)
+    if float_input == 1:
+        x_rgb = np.float32(x_corrupted / 255.0)
+    else:
+        x_rgb = np.uint8(x_corrupted)
+
+    if len(x[0,0]) == 1:
+        output = rgb2gray(x_rgb)
+        output =output[:, :, np.newaxis]
+    else:
+        output = x_rgb
+
+
+    return output
