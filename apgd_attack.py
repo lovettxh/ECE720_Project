0cfd078dde539080f39715806f3017bc4033c622
lovettxh <60330069+lovettxh@users.noreply.github.com>
2022-03-24 19:09:40 -0600

    Update apgd_attack.py

--- apgd_attack.py
+++ apgd_attack.py
@@ -8,15 +8,28 @@ class APGD_attack():
         self.device = device
         self.iter_step = iter_step
         self.epsilon = epsilon
+        self.restarts = 1
     def setup(self, x):
         self.dims = len(list(x.shape[1:]))
         self.iter_step2 = max(int(0.22 * self.iter_step),1)
+        self.iter_decr = max(int(0.03 * self.iter_step), 1)
+        self.iter_min = max(int(0.06 * self.n_iter), 1)
+
 
     def normalize(self, x):
         t = x.abs().view(x.shape[0], -1).max(1)[0]
         return x / (t.view(-1, *([1] * self.ndims)) + 1e-12)
 
+    def check_oscillation(self, x, j, k, y5, k3=0.75):
+        t = torch.zeros(x.shape[1]).to(self.device)
+        for counter5 in range(k):
+          t += (x[j - counter5] > x[j - counter5 - 1]).float()
+
     def apgd_attack(self,x,y):
+
+
+
+        loss_steps = torch.zeros([self.iter_step, x.shape[0]]).to(self.device)
         # t = 2 * torch.rand(x.shape).to(self.device).detach() - 1
         # x_adv = x + self.eps * torch.ones_like(x
         #         ).detach() * self.normalize(t)
@@ -40,7 +53,8 @@ class APGD_attack():
         alpha = 2
         step_size = alpha * self.epsilon * torch.ones([x.shape[0], *([1]*self.dims)]).to(self.device).detach()  # ???
         x_adv_old = x_adv.clone()
-
+        count = 0
+        iter_s = self.iter_step2 
         loss_best_save = loss_best.clone()
         reduced_save = torch.ones_like(loss_best)           # ???
 
@@ -73,4 +87,48 @@ class APGD_attack():
 
             ### check step size
             with torch.no_grad():
-                
+                loss_ = loss.detach().clone()
+                loss_steps[i] = loss_
+                idx = (loss_ > loss_best).nonzero().squeeze()
+                x_best[idx] = x_adv[idx].clone()
+                grad_best[idx] = grad[idx].clone()
+                loss_best[idx] = loss_[idx]
+                count += 1
+
+                if count == self.iter_s:
+                    oscillation = self.check_oscillation(loss_steps, i, self.iter_s, loss_best, k3=self.thr_decr)
+                    reduce_no_impr = (1. - reduced_save) * (loss_best_save >= loss_best).float()
+                    oscillation = torch.max(oscillation, reduce_no_impr)
+                    reduced_save = oscillation.clone()
+                    loss_best_save = loss_best.clone()
+
+                    if oscillation.sum() > 0:
+                        osc_idx = (oscillation > 0).nonzero().squeeze()
+                        step_size[osc_idx] /= 2
+                        x_adv[osc_idx] = x_best[osc_idx].clone()
+                        grad[osc_idx] = grad_best[osc_idx].clone()
+
+                    iter_s = max(iter_s - self.iter_decr, self.iter_min)
+                    count = 0
+
+        return (x_best, acc, loss_best, x_best_adv)
+
+    def eval(self, x, y):
+        self.setup(x)
+        x = x.detach().clone().float().to(self.device)
+        pred = self.model(x).max(1)[1]
+        y = y.detach().clone().float().to(self.device)
+        x_adv = x.detach().clone()
+        acc = pred == y
+
+        torch.random.manual_seed(self.seed)
+        torch.cuda.random.manual_seed(self.seed)
+
+        for i in range(self.restarts):
+            idx = acc.nonzero().squeeze()
+            if len(idx.shape) == 0:
+                idx = idx.unsqueeze(0)
+            if idx.numel() != 0:
+                x_ = x[idx].clone()
+                y_ = y[idx].clone()
+            
