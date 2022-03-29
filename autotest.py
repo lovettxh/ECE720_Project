690a7ffc82150560601143608f3d7faa5f707090
lovettxh <60330069+lovettxh@users.noreply.github.com>
2022-03-29 01:26:10 -0600

    update

--- autotest.py
+++ autotest.py
@@ -8,28 +8,26 @@ class autotest():
     def __init__(self, model, device):
         self.model = model
         self.device = device
-
+        self.steps = 100
     def run_normal_mode(self, x, y, batch_size, com_num, epsilon = 0.003):
-        apgd = APGD_attack(self.model, self.device, 100, epsilon)
-        fab = FAB_Attack_PT(self.model, n_restarts=5, n_iter=100, eps=epsilon, device=self.device)
+        apgd = APGD_attack(self.model, self.device, self.steps, epsilon)
+        fab = FAB_Attack_PT(self.model, n_restarts=5, n_iter=self.steps, eps=epsilon, device=self.device)
         c_a = Common_attack(1)
         robust_flags = torch.zeros(x.shape[0], dtype=torch.bool, device=self.device)
         y_adv = torch.empty_like(y)
         batch_num = int(np.ceil(x.shape[0] / batch_size))
+        # narutal acc
         for idx in range(batch_num):
             start_idx = batch_size * idx
             end_idx = min((idx + 1) * batch_size, x.shape[0])
-
             x_ = x[start_idx:end_idx, :].clone().to(self.device)
             y_ = y[start_idx:end_idx].clone().to(self.device)
             output = self.model(x_).max(dim=1)[1]
             y_adv[start_idx:end_idx] = output
             correct_batch = y_.eq(output)
             robust_flags[start_idx:end_idx] = correct_batch.detach()
-
         robust_accuracy = torch.sum(robust_flags).item() / x.shape[0] 
         print('initial accuracy: {:.2%}'.format(robust_accuracy))
-
         num_robust = torch.sum(robust_flags).item()
         if num_robust == 0:
             return
@@ -37,6 +35,7 @@ class autotest():
         robust_lin_idcs = torch.nonzero(robust_flags, as_tuple=False)
         if num_robust > 1:
             robust_lin_idcs.squeeze_()
+        # common
         for i in com_num:
             for idx in range(batch_num):
                 start_idx = batch_size * idx
@@ -48,7 +47,8 @@ class autotest():
                 y_ = y[batch_datapoint_idcs].clone().to(self.device)
                 if len(x_.shape) == 3:
                     x_.unsqueeze_(dim=0)
-                corrupt_image = torch.tensor(c_a.eval(x_.cpu().detach().numpy(), i)).to(self.device)
+                corrupt_image = torch.tensor(c_a.eval(x_.cpu().detach().numpy(), i)).type(torch.FloatTensor).to(self.device)
+
                 output = self.model(corrupt_image).max(dim=1)[1]
                 false_batch = ~y_.eq(output).to(robust_flags.device)
                 non_robust_lin_idcs = batch_datapoint_idcs[false_batch]
@@ -66,6 +66,10 @@ class autotest():
             if num_robust > 1:
                 robust_lin_idcs.squeeze_()
             # --
+        
+        # apgd
+        norm = 'Linf'
+        loss = 'ce'
         for idx in range(batch_num):
             start_idx = batch_size * idx
             end_idx = min((idx + 1) * batch_size, num_robust)
@@ -76,16 +80,35 @@ class autotest():
             y_ = y[batch_datapoint_idcs].clone().to(self.device)
             if len(x_.shape) == 3:
                 x_.unsqueeze_(dim=0)
-            adv_curr = fab.perturb(x_, y_)
+            adv_curr = apgd.eval(x_, y_, norm, loss)
             output = self.model(adv_curr).max(dim=1)[1]
             false_batch = ~y_.eq(output).to(robust_flags.device)
             non_robust_lin_idcs = batch_datapoint_idcs[false_batch]
             robust_flags[non_robust_lin_idcs] = False
             num_non_robust_batch = torch.sum(false_batch)
-            print('{} - {}/{} - {} out of {} successfully perturbed'.format('fab-Linf', idx + 1, batch_num, num_non_robust_batch, x_.shape[0]))
+            print('{} - {}/{} - {} out of {} successfully perturbed'.format('apgd-'+loss+'-'+norm, idx + 1, batch_num, num_non_robust_batch, x_.shape[0]))
+
+
+    def run_adv_mode(self, x, y, batch_size, com_num, epsilon = 0.03):
+        apgd = APGD_attack(self.model, self.device, self.steps, epsilon)
+        fab = FAB_Attack_PT(self.model, n_restarts=5, n_iter=self.steps, eps=epsilon, device=self.device)
+        c_a = Common_attack(1)
+        robust_flags = torch.zeros(x.shape[0], dtype=torch.bool, device=self.device)
+        y_adv = torch.empty_like(y)
+        batch_num = int(np.ceil(x.shape[0] / batch_size))
+        # natural acc
+        for idx in range(batch_num):
+            start_idx = batch_size * idx
+            end_idx = min((idx + 1) * batch_size, x.shape[0])
+            x_ = x[start_idx:end_idx, :].clone().to(self.device)
+            y_ = y[start_idx:end_idx].clone().to(self.device)
+            output = self.model(x_).max(dim=1)[1]
+            y_adv[start_idx:end_idx] = output
+            correct_batch = y_.eq(output)
+            robust_flags[start_idx:end_idx] = correct_batch.detach()
+        robust_accuracy = torch.sum(robust_flags).item() / x.shape[0] 
+        print('initial accuracy: {:.2%}'.format(robust_accuracy))
         num_robust = torch.sum(robust_flags).item()
-        robust_accuracy = torch.sum(robust_flags).item() / x.shape[0]
-        print(('robust accuracy after {}: {:.2%} '.format('apgd-'+loss+'-'+norm, robust_accuracy)))
         if num_robust == 0:
             return
         batch_num = int(np.ceil(num_robust / batch_size))
@@ -93,6 +116,29 @@ class autotest():
         if num_robust > 1:
             robust_lin_idcs.squeeze_()
         
+        for idx in range(batch_num):
+            start_idx = batch_size * idx
+            end_idx = min((idx + 1) * batch_size, num_robust)
+            batch_datapoint_idcs = robust_lin_idcs[start_idx:end_idx]
+            if len(batch_datapoint_idcs.shape) > 1:
+                batch_datapoint_idcs.squeeze_(-1)
+            x_ = x[batch_datapoint_idcs, :].clone().to(self.device)
+            y_ = y[batch_datapoint_idcs].clone().to(self.device)
+            if len(x_.shape) == 3:
+                x_.unsqueeze_(dim=0)
+            corrupt_image = torch.tensor(c_a.eval(x_.cpu().detach().numpy(), com_num)).type(torch.FloatTensor).to(self.device)
+            output = self.model(corrupt_image).max(dim=1)[1]
+            false_batch = ~y_.eq(output).to(robust_flags.device)
+            non_robust_lin_idcs = batch_datapoint_idcs[false_batch]
+            robust_flags[non_robust_lin_idcs] = False
+            num_non_robust_batch = torch.sum(false_batch)
+            print('{} - {}/{} - {} out of {} successfully perturbed'.format('common-'+str(com_num), idx + 1, batch_num, num_non_robust_batch, x_.shape[0]))
+            # update num
+        num_robust = torch.sum(robust_flags).item()
+        robust_accuracy = torch.sum(robust_flags).item() / x.shape[0]
+        print(('robust accuracy after {}: {:.2%} '.format('common-'+str(com_num), robust_accuracy)))
+
+        #apgd
         norm = 'Linf'
         for loss in ['ce', 'dlr']:
             for idx in range(batch_num):
@@ -121,13 +167,30 @@ class autotest():
             robust_lin_idcs = torch.nonzero(robust_flags, as_tuple=False)
             if num_robust > 1:
                 robust_lin_idcs.squeeze_()
+        # fab
+        for idx in range(batch_num):
+            start_idx = batch_size * idx
+            end_idx = min((idx + 1) * batch_size, num_robust)
+            batch_datapoint_idcs = robust_lin_idcs[start_idx:end_idx]
+            if len(batch_datapoint_idcs.shape) > 1:
+                batch_datapoint_idcs.squeeze_(-1)
+            x_ = x[batch_datapoint_idcs, :].clone().to(self.device)
+            y_ = y[batch_datapoint_idcs].clone().to(self.device)
+            if len(x_.shape) == 3:
+                x_.unsqueeze_(dim=0)
+            adv_curr = fab.perturb(x_, y_)
+            output = self.model(adv_curr).max(dim=1)[1]
+            false_batch = ~y_.eq(output).to(robust_flags.device)
+            non_robust_lin_idcs = batch_datapoint_idcs[false_batch]
+            robust_flags[non_robust_lin_idcs] = False
+            num_non_robust_batch = torch.sum(false_batch)
+            print('{} - {}/{} - {} out of {} successfully perturbed'.format('fab-Linf', idx + 1, batch_num, num_non_robust_batch, x_.shape[0]))
 
-    
-    def run_test(self, x, y, mode, epsilon , batch_size = 200):
-        com_num = list(range(22))
-        com_num.remove(5)
-        com_num.remove(12)
-        com_num.remove(17)
+
+    def run_test(self, x, y, mode, epsilon = 0.003 , batch_size = 200):
+        com_num = [0,1,2,3,4,5,6,13,14,15,16]
         #com_num = com_num[::-1]
         if mode == 'natural':
-            self.run_normal_mode(x, y, batch_size, [5], epsilon)
+            self.run_normal_mode(x, y, batch_size, com_num, 0.003)
+        elif mode == 'adv':
+            self.run_adv_mode(x,y, batch_size, [0], epsilon)
