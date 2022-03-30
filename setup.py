ec3085a4fdb594d729242a3a62478314ca5c865d
lovettxh <60330069+lovettxh@users.noreply.github.com>
2022-03-30 14:21:47 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+# or
+# from distutils.core import setup
+
+setup(
+        name='autotest',  
+        version='1.0',  
+        description='ECE720 project autotest system', 
+        author='Hao Xuan, Yinsheng He', 
+        author_email='hxuan@ualberta.ca, yinsheng@ualberta.ca',
+        url='https://github.com/lovettxh/ECE720_Project', 
+        packages=['autotest'], 
+)
