8fdce5937aa8a4a6ab2e71e912556a5350f27edf
lovettxh <60330069+lovettxh@users.noreply.github.com>
2022-03-30 22:56:58 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
         author='Hao Xuan, Yinsheng He', 
         author_email='hxuan@ualberta.ca, yinsheng@ualberta.ca',
         url='https://github.com/lovettxh/ECE720_Project', 
-        packages=['autotest'], 
+        packages=['imagenet_c'], 
 )
